0eba6596bc57f79791baead8151bd6d080f1ab0d
Renuka <renukavipat14@gmail.com>
2024-06-13 15:28:21 +0530

    done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"//treat all js code as newer code version
+alert("hello")// we are using nodejs not browser
+// code readability should be high
+
+
+let name = "renuka "//string dattype
+let age =18//number
+let ligin = true//boolean
+
+
+
+//number => 2 to power 53
+//big int (for very large number)
+//string -> mostly used with double quotes
+//boolean => true/false
+//null => standalone value yeh apne me ek dat type hai (empty)
+//undefined=> variable assign hua hai butuski value nhi ahi 
+//symbol => to find uniqnesss(mostly used on react)
+
+
+
+// console.log(typeof "renuka")
+//     output is string 
+// typeof gives type of the Input
+
+
+// console.log(typeof null);
+// output Object(special case)
+
+
+// same with undefinerd but its type is undefined itself
+
+
---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,20 @@
+// let score = 33;//number
+// let score = "33";//string
+// let score ="33abs";//NaN
+// let score = null; //0
+// let score =  undefined//NaN
+// let score = true/false // 1/0
+// console.log(typeof score);
+// console.log(typeof(score));//as method
+
+//"33"=>33 string to number
+// "33abc"=> NaN
+// "true" => 1 ; false => 0
+//""=>false 
+//"renuka"=>true
+//conversion to boolean value
+// let isLoggedIn= "1"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)//will give output as true
+//let isLoggedIn = "";
+//empty will give false value
+//same as converted to string also
